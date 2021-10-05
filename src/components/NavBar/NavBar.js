ab2b82a86b04fc68439f14f0b2c7a18a2a1199a1
dabin <dabin219@naver.com>
2021-10-06 01:15:37 +0900

    Fix: map과 Link 추가

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import category from './category';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGripLines } from '@fortawesome/free-solid-svg-icons';
 import { faBox } from '@fortawesome/free-solid-svg-icons';
@@ -7,156 +8,65 @@ import './NavBar.scss';
 
 class NavBar extends Component {
   render() {
+    const navbarList = [
+      { name: '베스트', link: 'bestproducts' },
+      { name: '특가', link: 'specialprice' },
+      { name: '신상품', link: 'newproducts' },
+      { name: '식단플랜', link: 'dietplan' },
+      { name: '다신배송', link: 'dashindelivery' },
+      { name: '다신쿨배송', link: 'dashincooldelivery' },
+    ];
     return (
-      //수정수정
-      <>
-        <nav className="NavBar">
-          <div className="navbarWrap">
-            <div className="navbar">
-              <ul className="bar">
-                <li className="dropDown">
-                  <div className="category">
-                    <FontAwesomeIcon icon={faGripLines} />
-                    <p> 전체 카테고리 </p>
-                  </div>
-                  <div className="dropDownCategory">
-                    <ul className="mainMenu">
-                      <li className="mainMenuList">
-                        <a href="/">다신메이드</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">다신메이드</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">도시락과 샐러드</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">도시락</a>
-                          </li>
-                          <li>
-                            <a href="/">샐러드</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">닭가슴살</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">닭가슴살</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">간편식</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">베이커리/떡</a>
-                          </li>
-                          <li>
-                            <a href="/">분식</a>
-                          </li>
-                          <li>
-                            <a href="/">곤약/면류</a>
-                          </li>
-                          <li>
-                            <a href="/">시리얼/선식</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">건강간식</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">건강과자</a>
-                          </li>
-                          <li>
-                            <a href="/">고구마/단호박/계란</a>
-                          </li>
-                          <li>
-                            <a href="/">디저트</a>
-                          </li>
-                          <li>
-                            <a href="/">견과/바</a>
-                          </li>
-                          <li>
-                            <a href="/">소스/잼</a>
-                          </li>
-                          <li>
-                            <a href="/">두유/요거트/차</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">단백질/헬스</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">단백질보충</a>
-                          </li>
-                          <li>
-                            <a href="/">장건강</a>
-                          </li>
-                          <li>
-                            <a href="/">건강식품</a>
-                          </li>
-                          <li>
-                            <a href="/">체중계/홈트</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">체지방관리</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">체지방</a>
-                          </li>
-                          <li>
-                            <a href="/">크렌즈/스무디</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="mainMenuList">
-                        <a href="/">익선동18</a>
-                        <ul className="subMenu">
-                          <li>
-                            <a href="/">익선동18</a>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">베스트</li>
-                </Link>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">특가</li>
-                </Link>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">신상품</li>
-                </Link>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">식단플랜</li>
-                </Link>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">
-                    <FontAwesomeIcon icon={faBox} />
-                    &nbsp;다신배송
-                  </li>
-                </Link>
-                <Link to="/">
-                  <li className="topMenuWithoutCategory">
-                    <FontAwesomeIcon icon={faBox} />
-                    &nbsp;다신쿨배송
-                  </li>
-                </Link>
-                <li className="blank"></li>
-              </ul>
-            </div>
+      <nav className="NavBar">
+        <div className="navbarWrap">
+          <div className="navbar">
+            <ul className="bar">
+              <li className="dropDown">
+                <div className="category">
+                  <FontAwesomeIcon icon={faGripLines} />
+                  <p> 전체 카테고리 </p>
+                </div>
+                <div className="dropDownCategory">
+                  <ul className="mainMenu">
+                    {category.map((data, id) => {
+                      const name = '/list/' + data.name;
+                      return (
+                        <li className="mainMenuList" key={id}>
+                          <Link to={name}>{data.name}</Link>
+                          <ul className="subMenu">
+                            {data.list.map((list, id) => {
+                              const name = '/list/' + list;
+                              return (
+                                <li key={id}>
+                                  <Link to={name}>{list}</Link>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </li>
+              {navbarList.map((list, id) => {
+                const name = '/list/' + list.link;
+                return (
+                  <Link to={name} key={id}>
+                    <li>
+                      {list.name.includes('배송') && (
+                        <FontAwesomeIcon icon={faBox} />
+                      )}
+                      &nbsp;{list.name}
+                    </li>
+                  </Link>
+                );
+              })}
+              <li className="blank"></li>
+            </ul>
           </div>
-        </nav>
-      </>
+        </div>
+      </nav>
     );
   }
 }
