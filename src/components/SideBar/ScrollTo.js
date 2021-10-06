db8ed73efd8680ea9cc0326979f78070567c2935
dabin <dabin219@naver.com>
2021-10-06 12:34:21 +0900

    Fix: 클래스형 컴포넌트로 변경, 목데이터 적용(로직 수정 필요)

---
+++ src/components/SideBar/ScrollTo.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class ScrollTo extends Component {
+  constructor() {
+    super();
+    this.myRef = React.createRef();
+  }
+
+  scrollToTop = () => {
+    document.documentElement.scrollTop = 0;
+  };
+
+  scrollToBottom = () => {
+    document.documentElement.scrollTo(0, document.body.scrollHeight);
+  };
+
+  render() {
+    return (
+      <div className="ScrollTo">
+        <button onClick={this.scrollToTop}>∧</button>
+        <button onClick={this.scrollToBottom}>∨</button>
+      </div>
+    );
+  }
+}
+
+export default ScrollTo;
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,64 +1,86 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import potato from './감자.jpeg';
-import sweetpotato from './sweetpotato.jpeg';
+import ScrollTo from './ScrollTo';
 import './SideBar.scss';
 
-const SideBar = () => {
-  const TOTAL_SLIDES = 4;
-  const [currentSlide, setCurrentSlide] = useState(1);
-  const slideRef = useRef(null);
-  const nextSlide = () => {
-    if (currentSlide >= TOTAL_SLIDES) {
-      setCurrentSlide(1);
-    } else {
-      setCurrentSlide(currentSlide + 1);
-    }
-  };
-  const prevSlide = () => {
-    if (currentSlide === 1) {
-      setCurrentSlide(TOTAL_SLIDES);
-    } else {
-      setCurrentSlide(currentSlide - 1);
-    }
-  };
-
-  const [scrollY, setScrollY] = useState(0);
-  const [scrollActive, setScrollActive] = useState(false);
-  const handleScroll = () => {
-    if (scrollY > 200) {
-      setScrollY(window.pageYOffset);
-      setScrollActive(true);
-    } else {
-      setScrollY(window.pageYOffset);
-      setScrollActive(false);
-    }
-  };
-  const scrollToTop = () => {
-    document.documentElement.scrollTop = 0;
-  };
-
-  const scrollToBottom = () => {
-    document.documentElement.scrollTo(0, document.body.scrollHeight);
-  };
-  useEffect(() => {
-    const scrollListener = () => {
-      window.addEventListener('scroll', handleScroll);
+class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebar: [],
+      totalSlides: 4,
+      currentSlide: 1,
+      scrollY: 0,
+      scrollActive: false,
     };
-    scrollListener(); // window 에서 스크롤을 감시
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }; //  window 에서 스크롤을 감시를 종료
-  });
-  const moveImg = (currentSlide - 1) * 80;
-  useEffect(() => {
-    slideRef.current.style.transform = `translateX(-${moveImg}px)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
-  }, [moveImg]);
+    this.myRef = React.createRef();
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/data/currentviewed.json', {
+      headers: {
+        Accpet: 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          sidebar: data.DATA,
+        });
+      });
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  nextSlide = () => {
+    if (this.state.currentSlide >= this.state.totalSlides) {
+      this.setState({
+        currentSlide: 1,
+      });
+    } else {
+      const nextSlide = this.state.currentSlide + 1;
+      this.setState({
+        currentSlide: nextSlide,
+      });
+    }
+    const moveImg = (this.state.currentSlide - 1) * 80;
+    this.myRef.current.style.transform = `translateX(-${moveImg}px)`;
+  };
 
-  return (
-    <>
-      <div className={scrollActive ? 'SideBar' : 'SideBarHide'}>
-        <div className="box">
+  prevSlide = () => {
+    if (this.state.currentSlide === 1) {
+      this.setState({
+        currentSlide: this.state.totalSlides,
+      });
+    } else {
+      const prevSlide = this.state.currentSlide - 1;
+      this.setState({
+        currentSlide: prevSlide,
+      });
+    }
+    const moveImg = (this.state.currentSlide - 1) * 80;
+    this.myRef.current.style.transform = `translateX(-${moveImg}px)`;
+  };
+
+  handleScroll = () => {
+    const { scrollY } = this.state;
+    if (scrollY > 200) {
+      this.setState({
+        scrollY: window.pageYOffset,
+        scrollActive: true,
+      });
+    } else {
+      this.setState({
+        scrollY: window.pageYOffset,
+        scrollActive: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <aside className={this.state.scrollActive ? 'SideBar' : 'SideBarHide'}>
+        <div className="sidebarWrap">
           <ul>
             <li>
               <Link to="/">
@@ -68,74 +90,35 @@ const SideBar = () => {
             <li>
               <div className="current">최근본상품</div>
             </li>
-            <li className="imgBox" ref={slideRef}>
+            <li className="imgBox" ref={this.myRef}>
               <div className="currentImg">
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={potato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
-                <Link to="/" className="linkToProduct">
-                  <img alt="" src={sweetpotato} />
-                </Link>
+                {this.state.sidebar.map((data, id) => {
+                  return (
+                    <Link
+                      key={id}
+                      to={`/product/${data.id}`}
+                      className="imgWrap"
+                    >
+                      <img alt="" src={data.img} />
+                    </Link>
+                  );
+                })}
               </div>
             </li>
-            <li className="imgListNum">
-              <div className="button">
-                <button onClick={prevSlide}>&lt;</button>
-                <div>{currentSlide}/4</div>
-                <button onClick={nextSlide}>&gt;</button>
+            <li className="listNumAndButtonWrap">
+              <div className="listNumAndButton">
+                {/* 제품 있을 때만 클릭 가능 로직 추가해야함 */}
+                <button onClick={this.prevSlide}>&lt;</button>
+                <div>{this.state.currentSlide}/4</div>
+                <button onClick={this.nextSlide}>&gt;</button>
               </div>
             </li>
           </ul>
         </div>
-        <div className="ScrollTo">
-          <button onClick={scrollToTop}>∧</button>
-          <button onClick={scrollToBottom}>∨</button>
-        </div>
-      </div>
-    </>
-  );
-};
+        <ScrollTo />
+      </aside>
+    );
+  }
+}
 
 export default SideBar;
