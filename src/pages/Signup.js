5c4e9e0da450fb458b4ff84ce7d5c7b1ee8f0089
jungho shin <junghoshin@junghoui-MacBookAir.local>
2021-10-08 10:32:49 +0900

    Fix: pages/signup에 nav header footer지우기, css 1차 push

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import SignUp from '../components/SignUp/SignUp';
-import Header from '../components/Header/Header';
-import NavBar from '../components/NavBar/NavBar';
-import Footer from '../components/Footer/Footer';
-
 export class Signup extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <NavBar />
         <SignUp />
-        <Footer />
       </div>
     );
   }
