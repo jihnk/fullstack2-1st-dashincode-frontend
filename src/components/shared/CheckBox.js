7b46115e1875d6733dc5aaa3f79693e91d87d699
goplanit19@gmail.com <goplanit19@gmail.com>
2021-10-13 15:52:47 +0900

    add: checkbox 수정 시작

--- src/components/shared/CheckBox.js
+++ src/components/shared/CheckBox.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './CheckBox.scss';
 
 class CheckBox extends Component {
@@ -9,19 +13,29 @@ class CheckBox extends Component {
     // 현재 체크박스를 label로 처리하다보니 원하지 않게 동작하고 있기에 일반 체크박스와 이미지 체크를 사용하도록 리팩토링해야함
     return (
       <div className="checkBoxWrap">
-        <input
+        <FontAwesomeIcon id={id} icon={faSquare} className="checkboxIcon">
+          <FontAwesomeIcon id={id} icon={faCheck} className="checkIcon" />
+        </FontAwesomeIcon>
+        <FontAwesomeIcon
+          id={id}
+          icon={faCheckSquare}
+          className="checkbox"
+          checked="false"
+        />
+        {/* <FontAwesomeIcon id={id} icon={faCheck} className="checkIcon" /> */}
+        {/* <input
           type={'checkbox'}
           className="checkBox"
           id={id}
-          // checked={
-          //   id === 0
-          //     ? allChecked
-          //     : e => (checkItems.includes(id) ? true : false)
-          // }
+          checked={
+            id === 0
+              ? allChecked
+              : e => (checkItems.includes(id) ? true : false)
+          }
           onClick={e => handleChecked(id)}
-          // checked={e => (checkItems.includes(id) ? true : false)}
-        />
-        <label for={id} />
+          checked={e => (checkItems.includes(id) ? true : false)}
+        /> */}
+        {/* <label for={id} /> */}
       </div>
     );
   }
